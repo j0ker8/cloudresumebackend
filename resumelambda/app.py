d9598508abcc75ebd43593277893969d46676f5d
Shardul Yadav <shardulyadav8@gmail.com>
2022-11-01 19:35:42 +0000

    Lambda code added.

--- resumelambda/app.py
+++ resumelambda/app.py
@@ -1,42 +1,24 @@
 import json
-
-# import requests
-
+import boto3
+dynamodb = boto3.resource('dynamodb');
+table = dynamodb.Table('visitorcounter');
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
+    # TODO implement
+    response=table.get_item(Key={"elementid": "primary"})
+    item=int(response["Item"]["vcount"]) + 1
+    print(item)
+    #vcount=response["vcount"];
+    table.update_item(
+    Key={
+        "elementid": "primary"
+    },
+    UpdateExpression='SET vcount= :val1',
+    ExpressionAttributeValues={
+        ':val1': item 
+    })
+    
     return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+        'statusCode': 200,
+        'body': item
     }
